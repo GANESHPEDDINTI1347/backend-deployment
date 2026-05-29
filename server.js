d795d928b41ede277faa834e1b06d00a39d59eb5
GANESHPEDDINTI1347 <ganeshpeddinti40@gmail.com>
2026-05-30 02:11:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -335,6 +335,13 @@ app.get("/adminStats", async (req, res) => {
 /* ---------- SERVER ---------- */
 const PORT = process.env.PORT || 5000;
 
+app.get("/test", (req, res) => {
+  res.json({
+    success: true,
+    database: "connected"
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
